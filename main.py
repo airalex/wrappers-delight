146d14bf90f2669b5ee9b805a3a07d3efa0860f7
Alexander Juda <alexander.juda@icloud.com>
2019-06-22 20:17:06 +0200

    Draw obstacles with range overflow

--- main.py
+++ main.py
@@ -38,26 +38,35 @@ def _read_desc(path):
 
     return {'mine_map': _parse_map_str(mine_map_str),
             'worker_pos': _parse_worker_pos(worker_pos_str),
-            'obstacles_pts': _parse_obstacles_str(obstacles_str),
+            'obstacles': _parse_obstacles_str(obstacles_str),
             'boosters_str': boosters_str}
+
+
+def _draw_state(im, draw_opts, state):
+    d_ctx = PIL.ImageDraw.Draw(im)
+    d_ctx.polygon(state['desc']['mine_map'], fill='white')
+    for obs_pts in state['desc']['obstacles']:
+        d_ctx.polygon(obs_pts, fill='gray')
+    d_ctx.point(state['desc']['worker_pos'], fill='red')
+
+
+def _export_im(im, draw_opts, path):
+    im = im.resize([s * draw_opts['render_scale'] for s in im.size])
+    im.save(path)
 
 
 def main():
     desc = _read_desc(_desc_path())
     pprint(desc)
 
-    render_scale = 8
     map_bbox = PIL.ImagePath.Path(desc['mine_map']).getbbox()
     map_size = [math.ceil(a) + 1
                 for a in [map_bbox[2] - map_bbox[0], map_bbox[3] - map_bbox[1]]]
     im = PIL.Image.new('RGBA', map_size)
 
-    d_ctx = PIL.ImageDraw.Draw(im)
-    d_ctx.polygon(desc['mine_map'], fill='white')
-    d_ctx.point(desc['worker_pos'], fill='red')
-
-    im = im.resize([s * render_scale for s in map_size])
-    im.save('data/output/sample.png')
+    draw_opts = {'render_scale': 1}
+    _draw_state(im, draw_opts, {'desc': desc})
+    _export_im(im, draw_opts, 'data/output/sample.png')
 
 
 if __name__ == '__main__':
